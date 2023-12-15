dd4e6b68eca853d9c12653c4de8b11343ac7688b
Lyrisbee <Lyrisbee@users.noreply.github.com>
2023-12-15 15:57:06 +0800

    lint

--- uninstall.php
+++ uninstall.php
@@ -1,8 +1,13 @@
 <?php
+/**
+ * Storipress
+ *
+ * @package Storipress
+ */
 
-// if uninstall.php is not called by WordPress, die
+// If uninstall.php is not called by WordPress, die.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    die;
+	die;
 }
 
 require_once __DIR__ . '/autoload.php';
