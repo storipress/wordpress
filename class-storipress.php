daffb66f07375d6e760fbdea932cdc03313a6a80
bepsvpt <8221099+bepsvpt@users.noreply.github.com>
2024-02-02 10:26:43 +0800

    chore: release 0.0.14

--- class-storipress.php
+++ class-storipress.php
@@ -20,7 +20,7 @@ final class Storipress {
 	 *
 	 * @since 0.0.2
 	 */
-	public $version = '0.0.13';
+	public $version = '0.0.14';
 
 	/**
 	 * Plugin build version.
@@ -29,7 +29,7 @@ final class Storipress {
 	 *
 	 * @since 0.0.10
 	 */
-	protected $build = 13;
+	protected $build = 14;
 
 	/**
 	 * Instance of this class.
